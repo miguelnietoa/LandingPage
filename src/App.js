880bd985bee9eb352673ea33fb8c06d1de3e3880
Miguel Nieto A <anietom@uninorte.edu.co>
2021-02-16 23:35:07 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Navbar from "./components/Navbar/Navbar";
 import banner from "./assets/2219.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <img src={banner} className="banner"></img>
-      <h1 style={{ textAlign: "center" }}>See Curata in action</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <img src={banner} className="banner"></img>
+            <h1 style={{ textAlign: "center" }}>See Curata in action</h1>
+        </div>
+    );
 }
 
 export default App;
